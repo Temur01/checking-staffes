2521591be2280a16f0341abdaf1e35bf382388f4
temur <suvonovtemur2008@gmail.com>
2025-04-09 17:37:35 +0500

    pushing new changes

--- src/preload.js
+++ src/preload.js
@@ -1,13 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  // System info
   getMacAddress: () => ipcRenderer.invoke('get-mac-address'),
   
-  // Navigation
   goToFaceID: () => ipcRenderer.invoke('go-to-faceid'),
   goToWelcome: () => ipcRenderer.invoke('go-to-welcome'),
   
-  // Face ID verification
   verificationSuccess: () => ipcRenderer.invoke('verification-success')
 });
