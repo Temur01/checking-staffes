39cfab762c73962f94af91c0613c166dd935d23c
temur <suvonovtemur2008@gmail.com>
2025-04-09 18:16:14 +0500

    making prettier

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const os = require('os');
-const axios = require('axios');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const os = require("os");
+const axios = require("axios");
 
 let mainWindow;
-let currentPage = 'welcome';
-const API_URL = 'https://nazorat.argos.uz/api';
+let currentPage = "welcome";
+const API_URL = "https://nazorat.argos.uz/api";
 
 function getEthernetMacAddress() {
   const interfaces = os.networkInterfaces();
-  const ifaceList = interfaces['Ethernet']; 
+  const ifaceList = interfaces["Ethernet"];
 
   if (!ifaceList) {
-    return 'Ethernet interfeysi topilmadi';
+    return "Ethernet interfeysi topilmadi";
   }
 
   for (const iface of ifaceList) {
-    if (!iface.internal && iface.mac && iface.mac !== '00:00:00:00:00:00') {
+    if (!iface.internal && iface.mac && iface.mac !== "00:00:00:00:00:00") {
       return iface.mac;
     }
   }
 
-  return 'Ethernet MAC topilmadi';
+  return "Ethernet MAC topilmadi";
 }
 
 function createMainWindow() {
@@ -29,77 +29,77 @@ function createMainWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
     autoHideMenuBar: true,
     resizable: true,
     center: true,
-    fullscreen: true
+    fullscreen: true,
   });
 
-  loadPage('welcome');
-  
-  mainWindow.webContents.on('will-navigate', (event, url) => {
-    if (!url.startsWith('file://')) {
+  loadPage("welcome");
+
+  mainWindow.webContents.on("will-navigate", (event, url) => {
+    if (!url.startsWith("file://")) {
       event.preventDefault();
     }
   });
-  
-  const { globalShortcut } = require('electron');
-  
-  globalShortcut.register('F11', () => {
+
+  const { globalShortcut } = require("electron");
+
+  globalShortcut.register("F11", () => {
     const isFullScreen = mainWindow.isFullScreen();
     mainWindow.setFullScreen(!isFullScreen);
   });
-  
-  globalShortcut.register('Escape', () => {
+
+  globalShortcut.register("Escape", () => {
     if (mainWindow.isFullScreen()) {
       mainWindow.setFullScreen(false);
     }
   });
-  
-  mainWindow.on('closed', () => {
+
+  mainWindow.on("closed", () => {
     globalShortcut.unregisterAll();
   });
 }
 
 function loadPage(pageName) {
   if (!mainWindow) return;
-  
+
   currentPage = pageName;
   const pagePath = path.join(__dirname, `${pageName}.html`);
   mainWindow.loadFile(pagePath);
 }
 
-ipcMain.handle('get-mac-address', async () => {
+ipcMain.handle("get-mac-address", async () => {
   return getEthernetMacAddress();
 });
 
-ipcMain.handle('check-mac-exists', async () => {
+ipcMain.handle("check-mac-exists", async () => {
   try {
     const macAddress = getEthernetMacAddress();
     const response = await axios.get(`${API_URL}/check-mac`, {
-      params: { mac: macAddress }
+      params: { mac: macAddress },
     });
     return response.data.is_exists;
   } catch {
-    return false; 
+    return false;
   }
 });
 
-ipcMain.handle('go-to-faceid', () => {
-  loadPage('faceid');
+ipcMain.handle("go-to-faceid", () => {
+  loadPage("faceid");
   return true;
 });
 
-ipcMain.handle('go-to-welcome', () => {
-  loadPage('welcome');
+ipcMain.handle("go-to-welcome", () => {
+  loadPage("welcome");
   return true;
 });
 
-ipcMain.handle('verification-success', () => {
+ipcMain.handle("verification-success", () => {
   setTimeout(() => {
-    loadPage('welcome');
+    loadPage("welcome");
   }, 2000);
   return true;
 });
@@ -107,15 +107,15 @@ ipcMain.handle('verification-success', () => {
 app.whenReady().then(() => {
   createMainWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createMainWindow();
     }
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
--- src/preload.js
+++ src/preload.js
@@ -1,11 +1,11 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  getMacAddress: () => ipcRenderer.invoke('get-mac-address'),
-  checkMacExists: () => ipcRenderer.invoke('check-mac-exists'),
-  
-  goToFaceID: () => ipcRenderer.invoke('go-to-faceid'),
-  goToWelcome: () => ipcRenderer.invoke('go-to-welcome'),
-  
-  verificationSuccess: () => ipcRenderer.invoke('verification-success')
+contextBridge.exposeInMainWorld("electronAPI", {
+  getMacAddress: () => ipcRenderer.invoke("get-mac-address"),
+  checkMacExists: () => ipcRenderer.invoke("check-mac-exists"),
+
+  goToFaceID: () => ipcRenderer.invoke("go-to-faceid"),
+  goToWelcome: () => ipcRenderer.invoke("go-to-welcome"),
+
+  verificationSuccess: () => ipcRenderer.invoke("verification-success"),
 });
